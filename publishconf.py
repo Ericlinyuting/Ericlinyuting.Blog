d149dee35966e34a0598da563884dd4f6267f55c
Eric <linyuting870511@gmail.com>
2023-12-18 22:41:31 +0800

    安裝ＧA前備份環境

--- publishconf.py
+++ publishconf.py
@@ -8,8 +8,18 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "YTLinTaiwan"
+SITEURL = "YTLin"
 RELATIVE_URLS = False
+
+# Parse content and save as JSON for searching
+PLUGINS += [
+    # 'tipue_search',
+    'pelican-ga-pageview',
+    'sitemap',
+    'share_post'
+]
+PLUGINS = list(set(PLUGINS))
+
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
@@ -19,4 +29,13 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 # DISQUS_SITENAME = ""
+
+# Google Page View Plugin setting
+# Reference: https://github.com/jhshi/pelican.plugins.ga_page_view
+GOOGLE_SERVICE_ACCOUNT = 'eric-s-blog@erics-blog-408106.iam.gserviceaccount.com'
+GOOGLE_KEY_FILE = './erics-blog-408106-476fb2b79b78.json'
+GA_START_DATE = '2023-12-01'
+GA_END_DATE = 'today'
+GA_METRIC = 'ga:pageviews'
+POPULAR_POST_START = 'A month ago'
 # GOOGLE_ANALYTICS = ""
